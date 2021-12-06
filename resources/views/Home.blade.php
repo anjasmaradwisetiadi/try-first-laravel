155571042482feb86b82068087226d12618724a9
AnjasmaraDS <88811992+AnjasmaraDS@users.noreply.github.com>
2021-12-06 13:58:24 +0700

    change home
    change home

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <h1>Home</h1>
-    <h4>Selamat Datang Anjasmara Dwi Setiadi :) </h4>
+    <h4>Selamat Datang Anjasmara Dwi Setiadi 07 :) </h4>
     <img src="image/gilford_legend.png" alt="gilford_legend" width="120px" height="120px">
 @endsection
